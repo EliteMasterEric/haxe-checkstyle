3097382a1aa272fccc6174f4ae070016f91ba005
EliteMasterEric <ericmyllyoja@gmail.com>
2026-03-01 22:13:50 -0500

    Resolve merge issues.

--- test/checkstyle/config/ConfigParserTest.hx
+++ test/checkstyle/config/ConfigParserTest.hx
@@ -4,7 +4,7 @@ import checkstyle.utils.ConfigUtils;
 
 class ConfigParserTest implements ITest {
 	static inline var LOCAL_PATH:String = "./";
-	static inline var TEST_COUNT:Int = 83;
+	static inline var TEST_COUNT:Int = 84;
 
 	public function new() {}
 
